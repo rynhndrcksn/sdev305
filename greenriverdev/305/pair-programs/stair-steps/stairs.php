95dc0b6eae682379e16ce61146f733542408e857
Ryan H <4751093-rynhndrcksn@users.noreply.gitlab.com>
2020-10-19 23:13:57 -0700

    downloaded avataaars, started portfolio project, making moves

---
+++ greenriverdev/305/pair-programs/stair-steps/stairs.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * Author: Ryan Hendrickson
+ * Date: October 19th, 2020.
+ * stairs.php is a file that creates a function that draws stairs.
+ */
+
+function stairsteps($numOfSteps) {
+	
+}
