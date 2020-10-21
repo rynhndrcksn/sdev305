fecfac770010adbda9d311cc614ea325f9a5e800
Ryan H <4751093-rynhndrcksn@users.noreply.gitlab.com>
2020-10-21 12:46:55 -0700

    Finished website for most part. Need to fine tune avatar size (it's too big currently for mobile) and maybe form info?

--- greenriverdev/305/php-practices/function_practice.php
+++ greenriverdev/305/php-practices/function_practice.php
@@ -1,12 +1,22 @@
 <?php
-	function averageOfTwo($num1, $num2) {
-		return ($num1 + $num2) / 2;
-	}
 
-	echo "<h2>The average is " . averageOfTwo(31, 1524) . "</h2>";
+// error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 
-	function circumference($radius) {
-		return (pi()*2) * $radius;
-	}
+// print server array | <pre> means preserve
+echo "<pre>";
+var_dump($_SERVER);
+echo "</pre>";
 
-	echo "<h2>The circumference is " . circumference(43.2) . "</h2>";
+function averageOfTwo($num1, $num2) {
+	return ($num1 + $num2) / 2;
+}
+
+echo "<h2>The average is " . averageOfTwo(31, 1524) . "</h2>";
+
+function circumference($radius) {
+	return (pi()*2) * $radius;
+}
+
+echo "<h2>The circumference is " . circumference(43.2) . "</h2>";
